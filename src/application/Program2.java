c6d263cb2cf0ebe43067a9078c496d158050d327
Diego Diniz <diegodiniz90@hotmail.com>
2022-06-16 22:42:01 -0300

    update & findById Department implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -5,6 +5,7 @@ import model.dao.DepartmentDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+
 import java.util.Date;
 
 public class Program2 {
@@ -15,6 +16,16 @@ public class Program2 {
         Department newDepartment = new Department(null, "Music");
         departmentDao.insert(newDepartment);
         System.out.println("Inserted! New id = " + newDepartment.getId());
+
+        System.out.println("\n ==== Test 2: Seller update ====");
+        Department department = departmentDao.findById(1);
+        department.setName("Food");
+        departmentDao.update(department);
+        System.out.println("Update completed!");
+
+        System.out.println("\n==== Test 3: Seller findById ====");
+        department = departmentDao.findById(3);
+        System.out.println(department);
     }
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -50,7 +50,22 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void update(Department obj) {
-
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "UPDATE Department "
+                    + "SET Name = ?"
+                    + "WHERE Id = ?");
+            st.setString(1, obj.getName());
+            st.setInt(2, obj.getId());
+            st.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
 
     }
 
@@ -61,7 +76,30 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public Department findById(Integer id) {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT department.* FROM department "
+                            +"WHERE department.Id = ?");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if(rs.next()){
+                Department obj = new Department();
+                obj.setId(rs.getInt("Id"));
+                obj.setName(rs.getString("Name"));
+                return obj;
+            }
+            return null;
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+
     }
 
     @Override
