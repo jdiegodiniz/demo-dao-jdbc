c5af2e52f3d18c20fc7f395a74d3add5e4dda1eb
Diego Diniz <diegodiniz90@hotmail.com>
2022-06-16 21:48:53 -0300

    inser Department implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,20 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
+public class Program2 {
+    public static void main(String[] args) {
+
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+        System.out.println("\n ==== Test 1 : Seller insert ====");
+        Department newDepartment = new Department(null, "Music");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id = " + newDepartment.getId());
+    }
+
+}
